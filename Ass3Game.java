7fc59ada41705763bd4768f81dea9637a39dc812
orie50 <turht50@gmail.com>
2017-07-19 09:56:10 +0300

    Add files via upload

---
+++ Ass3Game.java
@@ -0,0 +1,15 @@
+/**
+* @author Ori Engelberg <turht50@gmail.com>
+* @version 1.0
+* @since 2016-04-05 */
+public class Ass3Game {
+    /**
+    * Main function than run the game.
+    * <p>
+    * @param args - string of arguments to the main.*/
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.initialize();
+        game.run();
+        }
+}
